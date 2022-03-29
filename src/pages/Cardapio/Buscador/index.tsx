be63bcc87736f11189c13cdad9e65031846adbbd
wesley viana martins <wesley.vmartins.js@gmail.com>
2022-03-28 22:33:38 -0300

    Projeto aluroni

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import style from './Buscador.module.scss'
+import { CgSearch } from 'react-icons/cg';
+
+interface Props {
+  busca: string,
+  setBusca: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Buscador({busca, setBusca}: Props) {
+  return (
+    <div className={style.buscador}>
+      <input
+      value={busca}
+      onChange={evento => setBusca(evento.target.value)}
+      placeholder="Buscar"
+      />
+      <CgSearch size={20} color="#4C4D5E" />
+    </div>
+  )
+}
